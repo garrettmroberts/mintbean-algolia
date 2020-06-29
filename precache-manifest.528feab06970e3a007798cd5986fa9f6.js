78fa24023293eae16910c48d1995e0db9da3ec61
Garrett Roberts <garrettmroberts@gmail.com>
2020-06-29 11:05:36 -0500

    Updates

---
+++ precache-manifest.528feab06970e3a007798cd5986fa9f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a55eaf9fce4f679ad94c3f6d2497883",
+    "url": "/mintbean-algolia/index.html"
+  },
+  {
+    "revision": "abed02aebcb5c405c17c",
+    "url": "/mintbean-algolia/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1600f8cdf670ba8fab56",
+    "url": "/mintbean-algolia/static/js/2.577120d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mintbean-algolia/static/js/2.577120d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abed02aebcb5c405c17c",
+    "url": "/mintbean-algolia/static/js/main.b68aa2d4.chunk.js"
+  },
+  {
+    "revision": "38d8143e3efa3da99e2d",
+    "url": "/mintbean-algolia/static/js/runtime-main.656110ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mintbean-algolia/static/media/logo.5d5d9eef.svg"
+  }
+]);
